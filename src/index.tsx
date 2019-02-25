be5f713d498364127311e68cb826d23e65148b41
Origami ONOUE <onoueorigami@gmail.com>
2019-02-26 02:00:19 +0900

    はい

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+//import { Provider } from 'react-redux';
+import './static/reset.css'
+import 'react-virtualized/styles.css'
+import {App} from "./renderer/App";
+
+ReactDOM.render(
+        <App />,
+    document.getElementById("root")
+)
---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const App: React.FunctionComponent<{}> = () => {
+    return (
+        <div>
+            {"はい"}
+        </div>
+    )
+}
